cf1cf43097df22239bd989022ef0b57ca6818b7f
wilianbot <wiliamrobaldossantos1@gmail.com>
2024-12-29 17:28:15 -0300

    Finalizado about

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,6 +1,20 @@
 import Globe from "react-globe.gl";
+import Button from "../components/Button.jsx"
+import {useState} from "react";
 
 const About = () => {
+    const [hasCopied, setHasCopied] = useState(false);
+
+    const handleCopy = () => {
+        navigator.clipboard.writeText('wiliamrobaldossantos1@gmail.com');
+
+        setHasCopied(true);
+
+        setTimeout(() => {
+            setHasCopied(false);
+        }, 2000)
+    }
+
     return (
         <section className={"c-space my-20"}>
             <div className={"grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 grid-cols-1 gap-5 h-full"}>
@@ -39,12 +53,52 @@ const About = () => {
                                 globeImageUrl={'//unpkg.com/three-globe/example/img/earth-night.jpg'}
                                 bumpImageUrl={'//unpkg.com/three-globe/example/img/earth-topology.png'}
                                 labelsData={[{
-                                    lat: 40, lng: -100,
+                                    lat: -27, lng: -54,
                                     text: "Eu estou aqui",
                                     color: "white",
                                     size: 20,
                                 }]}
                             />
+                        </div>
+                        <div>
+                            <p className={"grid-headtext"}>
+                                Eu trabalho remotamente ou presencial se for na minha região.
+                            </p>
+                            <p className={"grid-subtext"}>
+                                Eu sou de Chapecó-SC, com disponibilidade para trabalho remoto.
+                            </p>
+                            <Button name={"Contate-me"} isBeam containerClass={"w-full mt-10"}/>
+                        </div>
+                    </div>
+                </div>
+
+                <div className={"xl:col-span-2 xl:row-span-3"}>
+                    <div className={"grid-container"}>
+                        <img src="/assets/grid3.png" alt="grid-3" className={"w-full sm:h-[266px] h-fit object-contain"}/>
+
+                        <div>
+                            <p className={"grid-headtext"}>
+                                Incrivel mundo da Programção
+                            </p>
+                            <p className={"grid-subtext"}>
+                                Só de eu saber que posso fazer qualquer coisa com codigos me inspira a aprender ainda mais sobre programção.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className={"xl:col-span-1 xl:row-span-2"}>
+                    <div className={"grid-container"}>
+                        <img src="/assets/grid4.png" alt="grid-4" className={"w-full md:h-[126px] sm:h-[276px] h-fit object-cover sm:object-top"}/>
+
+                        <div className={"space-y-2"}>
+                            <p className={"grid-subtext text-center"}>Contate-me</p>
+                            <div className={"copy-container"} onClick={handleCopy}>
+                                <img src={hasCopied ? 'assets/tick.svg' : 'assets/copy.svg'} alt="copy"/>
+                                <p className={"lg:text-xl sm:text-xs font-medium text-gray_gradient text-white"}>
+                                    wiliamrobaldossantos1@gmail.com
+                                </p>
+                            </div>
                         </div>
                     </div>
                 </div>
