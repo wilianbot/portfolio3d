1189ea86466c506a90e47376cb47f9f0eb11a1ae
wilianbot <wiliamrobaldossantos1@gmail.com>
2025-01-15 23:13:47 -0300

    create experience

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import About from "./sections/About.jsx"
 import Projects from "./sections/Projects.jsx"
 import Contact from "./sections/Contact.jsx"
 import Footer from "./sections/Footer.jsx"
+import Experience from "./sections/Experience.jsx"
 
 const App = () => {
     return (
@@ -12,6 +13,7 @@ const App = () => {
             <Hero />
             <About />
             <Projects />
+            <Experience />
             <Contact />
             <Footer />
         </main>
